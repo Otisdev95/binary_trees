0b51dec9f22f5e43d83bc43684572a8c6ba02a9a
otis <olaliotis95@gmail.com>
2023-06-01 00:14:53 -0700

    Binary trees

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,7 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - goes through a binary tree
+ *				using level-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
